0b105de2b37f46d303a3e9c16c971fa43f4df104
Andre <cruzandrecal@gmail.com>
2022-12-11 13:15:19 -0300

    Aula 2 - Atividades 01 a 05

---
+++ app/sortearNumero.js
@@ -0,0 +1,21 @@
+const menorValor = 1
+const maiorValor = 1000
+
+const numeroSecreto = gerarNumeroAleatorio()
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * maiorValor + 1)
+}
+
+console.log('Número secreto:',numeroSecreto)
+
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMaiorValor.innerHTML = maiorValor
+
+/**
+ * Mesmo resultado que o acima, só que em uma linha. Mas não guarda em uma variável.
+ * document.getElementById('maior-valor').innerHTML = maiorValor
+ */
